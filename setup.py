30ac29603b4f744a13440f10ebc5b4c600dc11b9
Tyler Potter <tjpotter@uvic.ca>
2016-03-20 22:53:00 -0700

    Add setup.py
    Add a basic setup.py to allow the project to correctly have a container built.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import (
+    setup,
+    find_packages
+    )
+
+setup(
+    name='udplugin',
+    version='0.1',
+    author='Tyler Potter',
+    author_email='tjpotter@uvic.ca',
+    description=(
+        'An application to be run on a Heroku server, or other server, '
+        'which can then be used as a Slack hook.'
+        ),
+    install_requires=['requests'],
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    )
